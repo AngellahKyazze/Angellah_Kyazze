3f7d73f281c1e724c88e41d11d92d85ec461d530
AngellahKyazze <angellakyazze8@gmail.com>
2024-10-31 18:11:00 +0300

    Add files via upload

---
+++ lists.js
@@ -0,0 +1,9 @@
+//friend list
+let friends = [
+    {fname,"Angellah",age:20}
+];
+//0r 
+let friend = [Angellah,Mary,]
+app.get('/list-of-friend',(request,response)=>{
+    return response.send(friend);
+});
