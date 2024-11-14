22a1e161ec4a4a03fef1fb7cfcfbec56ef2b03f7
AngellahKyazze <angellakyazze8@gmail.com>
2024-11-14 10:22:01 +0300

    Delete lists.js

--- lists.js
+++
@@ -1,9 +0,0 @@
-//friend list
-let friends = [
-    {fname,"Angellah",age:20}
-];
-//0r 
-let friend = [Angellah,Mary,]
-app.get('/list-of-friend',(request,response)=>{
-    return response.send(friend);
-});
